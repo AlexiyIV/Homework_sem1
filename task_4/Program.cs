291f364f1c397d26857bbf32884429cbbc15777d
Сучков Алексей <AlexiySV@yandex.ru>
2022-10-05 12:18:33 +0300

    task_4

---
+++ task_4/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите a=");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите b=");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Введите c=");
+int c = int.Parse(Console.ReadLine());
+int max = a;
+if ( a < b ){
+    max = b;
+}
+else if ( a < c ){
+    max = c;
+}
+if ( c > b ){
+    max = c;
+}
+Console.WriteLine( max );
