24eaaba726387f3b83915707b54e58eafea3d083
Сучков Алексей <AlexiySV@yandex.ru>
2022-10-05 11:50:59 +0300

    task_2

---
+++ task_2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите a=");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите b=");
+int b = int.Parse(Console.ReadLine());
+if ( a > b ){
+    Console.WriteLine("a больше");
+}
+else if ( a < b ){
+    Console.WriteLine("b больше");
+}
+else{
+    Console.WriteLine( "a и b равны");
+}
