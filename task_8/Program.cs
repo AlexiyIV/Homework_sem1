bf140cc8e8f1f2a067667f558e8d2ae24c9ade10
Сучков Алексей <AlexiySV@yandex.ru>
2022-10-05 12:47:37 +0300

    task_8

---
+++ task_8/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите N=");
+int N = int.Parse(Console.ReadLine());
+int count = 1;
+while ( count < N ){
+    if ( count % 2 == 0 ){
+        Console.Write( count + ", ");
+    }
+    count++;
+}
